b237739b625136c550579e84bafbc5c0a33e99c5
Bryan Soh <bryansoh95@gmail.com>
2021-06-30 17:04:41 +0800

    Added cors.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const body_parser = require('body-parser');
+const cors = require('cors');
 const routes = require('./app/routes');
 
 const app = express();
 
+app.use(cors());
+app.options('*', cors());
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
 app.use(routes);
